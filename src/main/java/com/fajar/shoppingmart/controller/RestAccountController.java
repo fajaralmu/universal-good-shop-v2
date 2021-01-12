a13a555bc8623bbb5de3c0e9b233616f4ef4ad3c
Fajar AM <somabangsa@gmail.com>
2021-01-12 21:16:45 +0700

    add endpoint updateProfile

--- src/main/java/com/fajar/shoppingmart/controller/RestAccountController.java
+++ src/main/java/com/fajar/shoppingmart/controller/RestAccountController.java
@@ -6,18 +6,22 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fajar.shoppingmart.dto.WebRequest;
 import com.fajar.shoppingmart.dto.WebResponse;
 import com.fajar.shoppingmart.entity.User;
 import com.fajar.shoppingmart.service.LogProxyFactory;
+import com.fajar.shoppingmart.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,6 +31,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RestAccountController extends BaseController {
 
+	@Autowired
+	private UserService userService;
 	public RestAccountController() {
 		log.info("------------------RestAccountController-----------------");
 	}
@@ -40,6 +46,10 @@ public class RestAccountController extends BaseController {
 	@PostMapping(value = "/user", produces = MediaType.APPLICATION_JSON_VALUE)
 	public User user(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException {
 		return sessionValidationService.getLoggedUser(httpRequest);
+	}
+	@PostMapping(value = "/updateprofile", produces = MediaType.APPLICATION_JSON_VALUE)
+	public WebResponse updateProfile(@RequestBody WebRequest webRequest, HttpServletRequest httpRequest) throws IOException {
+		return userService.updateProfile(httpRequest, webRequest);
 	}
 	@PostMapping(value="/logout", produces = MediaType.APPLICATION_JSON_VALUE)
     public WebResponse logout (HttpServletRequest request, HttpServletResponse response) {
